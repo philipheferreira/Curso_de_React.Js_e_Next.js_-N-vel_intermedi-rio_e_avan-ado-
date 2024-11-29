85f74bc071239e1dcb4f5dbbc417ea0986b1dec8
philipheferreira <philipheferreira@gmail.com>
2024-11-29 15:33:49 -0300

    Feat: teste1 delete fully things

--- testes/teste1/src/Components/App.js
+++ testes/teste1/src/Components/App.js
@@ -1,13 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Editar <code>src/App.js</code> e salvar para carregar.
         </p>
         <a
           className="App-link"
@@ -15,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Aprenda React
         </a>
       </header>
     </div>
