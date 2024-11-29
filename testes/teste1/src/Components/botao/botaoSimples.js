c68e5e88294a62c032699ecb82ec2e9586a1d148
philipheferreira <philipheferreira@gmail.com>
2024-11-29 16:40:39 -0300

    Feat: buttons create

---
+++ testes/teste1/src/Components/botao/botaoSimples.js
@@ -0,0 +1,11 @@
+import './botaoSimples.css'
+
+function botao1(){
+    return (
+        <div>
+            <button>Primeiro botão</button>
+        </div>
+    )
+}
+
+export default botao1
---
+++ testes/teste1/src/Components/botao/botaoSimples2.js
@@ -0,0 +1,11 @@
+import './botaoSimples2.css'
+
+export default function botao2(){
+    return (
+        <div>
+            <button>Segundo botão</button>
+        </div>
+    )
+}
+
+
