bd3f4a05558556cc847bf720c981a71c59618d13
philipheferreira <philipheferreira@gmail.com>
2024-12-02 09:58:30 -0300

    Feat: update testes

--- testes/teste1/src/Components/App.js
+++ testes/teste1/src/Components/App.js
@@ -1,22 +1,29 @@
 import './App.css';
-import Botao1 from './botao/botaoSimples';
-import Botao2 from './botao/botaoSimples2';
+import Botao1 from './botao/botaoSimples'; // Os botões tem que ser definidos como maiusculos depois do import
+import Botao2 from './botao/botaoSimples2'; // Os botões tem que ser definidos como maiusculos depois do import
+
+const pessoa = {
+  nome: 'Philiphe Siqueira Ferreira',
+  imgUrl: 'https://media.gq.com/photos/55828b3f1177d66d68d5287c/1:1/w_450,h_450,c_limit/blogs-the-feed-how-i-met-your-mother-barney-stinson.jpg',
+  imgHeight: 180,
+  imgWidth: 180
+};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Editar <code>src/App.js</code> e salvar para carregar.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Aprenda React
-        </a>
+        <h1>{pessoa.nome}</h1>
+        <img
+         className='ImagemProfile'
+         alt= { "Foto de " + pessoa.nome}
+         src= {pessoa.imgUrl}
+         style={{
+          width: pessoa.imgWidth,
+          height: pessoa.imgHeight
+         }}
+        />
+         
         <Botao1/>
         <Botao2/>
       </header>
