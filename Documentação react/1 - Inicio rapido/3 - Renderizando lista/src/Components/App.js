08f15b3381436eb01603bf0f7b4b0592d935f5f9
philipheferreira <philipheferreira@gmail.com>
2024-12-02 14:23:58 -0300

    Create: documetation project 3 List

---
+++ Documentação react/1 - Inicio rapido/3 - Renderizando lista/src/Components/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Lista from './Lista/ListaRenderizada'
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Lista></Lista>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ Documentação react/1 - Inicio rapido/3 - Renderizando lista/src/Components/Lista/ListaRenderizada.js
@@ -0,0 +1,15 @@
+import './ListaRenderizada.css'
+
+const pessoas = [
+    {nome: 'Philiphe', simOuNao: false, id: 1},
+    {nome: 'João', simOuNao: false, id: 2},
+    {nome: 'Jherson', simOuNao: true, id: 3},
+];
+
+function ListaRenderizada(){
+    return(
+        <div>Oi</div>
+    );
+}
+
+export default ListaRenderizada;
